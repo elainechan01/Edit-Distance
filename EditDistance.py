5c5d53135cf8f8c539f5d75c723286109821aa73
elainechan01 <elaineyunru.chan@wmich.edu>
2022-11-30 21:21:54 -0500

    Finalized

--- EditDistance.py
+++ EditDistance.py
@@ -1,7 +1,7 @@
 import copy
 
 class EditDistance:
-    def __init__(self, str1: str, str2: str, cDel: int, cIns: int, cRep: int) -> None:
+    def __init__(self, str1: str, str2: str, cDel: float, cIns: float, cRep: float) -> None:
         self.str1 = str1
         self.str2 = str2
         self.d = cDel
@@ -29,107 +29,106 @@ class EditDistance:
                 # condition 2: characters are not equivalent, find minimum cost of the adjacent top-left corner cells
                 else:
                     self.CostMatrix[c1][c2] = min(self.CostMatrix[c1-1][c2-1]+self.r,self.CostMatrix[c1-1][c2]+self.d,self.CostMatrix[c1][c2-1]+self.i)
-
         return self.CostMatrix
 
-    def FindDecisionSequence(self, row: int, col: int, sequence: list = None) -> list:
-        if sequence is None:
-            sequence = []
-        sequence = sequence + [self.CostMatrix[row][col]]
-        if row == 0 and col == 0:
-            self.sequences.append(sequence)
-        for path in self.FindNextPath(row, col):
-            self.FindDecisionSequence(path[0], path[1], sequence)
-
-    def FindNextPath(self, row: int, col: int):
-        next = []
-
-        # check for equivalent character
-        if self.str1[row-1] == self.str2[col-1]:
-            # check for top cell
-            if self.CostMatrix[row-1][col] == self.CostMatrix[row][col]:
-                next.append([row-1,col])
-            # check for top left cell
-            if self.CostMatrix[row-1][col-1] == self.CostMatrix[row][col]:
-                next.append([row-1,col-1])
-            # check for left cell
-            if self.CostMatrix[row][col-1] == self.CostMatrix[row][col]:
-                next.append([row,col-1])
-        
-        # check for non-equivalent character
-        # string to be converted is shorter 
-        if len(self.str1) < len(self.str2) and row == 0 and col > 0:
-            # check for left cell
-            if (self.CostMatrix[row][col-1] + self.d) == self.CostMatrix[row][col-1] and [row,col-1] not in next:
-                next.append([row,col-1])
-        # string to be converted is longer
-        elif len(self.str1) > len(self.str2) and row > 0 and col == 0:
-            # check for top cell
-            if (self.CostMatrix[row-1][col] + self.i) == self.CostMatrix[row][col] and [row-1,col] not in next:
-                next.append([row-1,col])
-        else:
-            # check for top cell
-            if (self.CostMatrix[row-1][col] + self.i) == self.CostMatrix[row][col] and [row-1,col] not in next:
-                next.append([row-1,col])
-            # check for top left cell
-            if (self.CostMatrix[row-1][col-1] + self.r) == self.CostMatrix[row][col] and [row-1,col-1] not in next:
-                next.append([row-1,col-1])
-            # check for left cell
-            if (self.CostMatrix[row][col-1] + self.d) == self.CostMatrix[row][col] and [row,col-1] not in next:
-                next.append([row,col-1])
-        return next
-
-    def backtrackSequence(self, i=None, j=None):
-        matrix = self.CostMatrix
-        StringA = self.str1
-        StringB = self.str2
-        if(i==None):
-            i = len(StringA)
-        if(j==None):
-            j = len(StringB)
-        if(i==0 and j==0):
-            return [[[],StringA,0]]
-        if(StringA[i-1] == StringB[j-1]):
-            paths = self.backtrackSequence(i-1,j-1)
-            return paths
-        else:
-            paths = []
-            if(matrix[i-1][j] + 1 == matrix[i][j] and i-1 >= 0):
-                allPaths = self.backtrackSequence(i-1,j)
+    def backtrackSequence(self, row:int=None, col:int=None):
+        # print(row,col)
+        if(row==None):
+            row = len(self.str1)
+        if(col==None):
+            col = len(self.str2)
+        """base case: return an array with these values
+           index 0: operation sequence
+           index 1: current edited string
+           index 2: position of edit
+           index 3: decision sequence"""
+        sequences = []
+        # base case
+        if(row==0 and col==0):
+            return [[[],self.str1,0,[[row,col]]]]
+        # elif((row-1==0 or row==0) and (col-1==0 or col==0)):
+        #     return [[[],self.str1,0,[[row,col]]]]
+        # condition 1: characters match
+        if(self.str1[row-1] == self.str2[col-1]):
+            allPaths = self.backtrackSequence(row-1,col-1)
+            # print(allPaths)
+            try:
                 for path in allPaths:
-                    cS = path[1]
-                    deviation = path[2]
-                    path[1] = cS[0:i+deviation-1] + cS[i+deviation:len(cS)]
-                    path[2] = deviation - 1
-                    #print(cS + " delete " + StringA[i-1] + " " + path[1] + " " + "("+str(i)+","+str(j)+")")
-                    path[0].append(cS + " delete " + StringA[i-1] + "("+str(i-1+deviation)+")")
-                paths.extend(allPaths)
-                #print('Paths',paths)
-            if(matrix[i][j-1] + 1 == matrix[i][j] and j-1 >= 0):
-                allPaths = self.backtrackSequence(i,j-1)
+                    path[3].append([row,col])
+                sequences.extend(allPaths)
+            except TypeError:
+                pass
+        # condition 2: characters don't match -> check for operation
+        # check for deletion operation (upward)
+        if(self.CostMatrix[row-1][col] + self.d == self.CostMatrix[row][col] and row >= 1):
+            allPaths = self.backtrackSequence(row-1,col)
+            # print(allPaths)
+            try:
                 for path in allPaths:
-                    cS = path[1]
-                    deviation = path[2]
-                    path[1] = cS[0:i+deviation] + StringB[j-1] + cS[i+deviation:len(cS)]
-                    path[2] = deviation + 1
-                    path[0].append(cS + " insert " + StringB[j-1] +  "("+str(i-1+deviation)+")")
-                paths.extend(allPaths)
-                #print('Paths',paths)
-            if(matrix[i-1][j-1] + 1 == matrix[i][j] and i-1 >= 0 and j-1 >= 0):
-                allPaths = self.backtrackSequence(i-1,j-1)
+                    currStr = path[1]
+                    editIndex = path[2]
+                    path[1] = currStr[0:row+editIndex-1] + currStr[row+editIndex:len(currStr)]
+                    path[2] = editIndex - 1
+                    path[0].append(currStr + " delete " + self.str1[row-1] + " at " + str(row+editIndex-1))
+                    path[3].append([row,col])
+                sequences.extend(allPaths)
+            except TypeError:
+                pass
+        elif(row == 1 and col == 0):
+            allPaths = self.backtrackSequence(row-1,col)
+            # print(allPaths)
+            try:
                 for path in allPaths:
-                    cS = path[1]
-                    deviation = path[2]
-                    path[1] = cS[0:i+deviation-1] + StringB[j-1] + cS[i+deviation:len(cS)]
-                    path[0].append(cS + " replace " + StringA[i-1]  + " with " + StringB[j-1] +  "("+str(i-1+deviation)+")")
-                paths.extend(allPaths)
-                #print('Paths',paths)
-            return paths
-
-    def listAllSequence(self):
-        allSequence = self.backtrackSequence()
-        for path in allSequence:
-            if(len(path[0])>0):
-                print(" -> ".join(path[0])," -> ",path[1])
-            else:
-                print(self.str1," -> ",self.str2)
+                    currStr = path[1]
+                    editIndex = path[2]
+                    path[1] = currStr[0:row+editIndex-1] + currStr[row+editIndex:len(currStr)]
+                    path[2] = editIndex - 1
+                    path[0].append(currStr + " delete " + self.str1[row-1] + " at " + str(row+editIndex-1))
+                    path[3].append([row,col])
+                sequences.extend(allPaths)
+            except TypeError:
+                pass
+        # check for insertion operation (leftward)
+        if(self.CostMatrix[row][col-1] + self.i == self.CostMatrix[row][col] and col >= 1):
+            allPaths = self.backtrackSequence(row,col-1)
+            # print(allPaths)
+            try:
+                for path in allPaths:
+                    currStr = path[1]
+                    editIndex = path[2]
+                    path[1] = currStr[0:row+editIndex] + self.str2[col-1] + currStr[row+editIndex:len(currStr)]
+                    path[2] = editIndex + 1
+                    path[0].append(currStr + " insert " + self.str2[col-1] +  " at " + str(row-1))
+                    path[3].append([row,col])
+                sequences.extend(allPaths)
+            except TypeError:
+                pass
+        elif(row == 0 and col == 1):
+            allPaths = self.backtrackSequence(row,col-1)
+            # print(allPaths)
+            try:
+                for path in allPaths:
+                    currStr = path[1]
+                    editIndex = path[2]
+                    path[1] = currStr[0:row+editIndex] + self.str2[col-1] + currStr[row+editIndex:len(currStr)]
+                    path[2] = editIndex + 1
+                    path[0].append(currStr + " insert " + self.str2[col-1] +  " at " + str(row-1))
+                    path[3].append([row,col])
+                sequences.extend(allPaths)
+            except TypeError:
+                pass
+        # check for replacement/change operation (diagonal)
+        if(self.CostMatrix[row-1][col-1] + self.r == self.CostMatrix[row][col] and row >= 1 and col >= 1):
+            allPaths = self.backtrackSequence(row-1,col-1)
+            # print(allPaths)
+            try:
+                for path in allPaths:
+                    currStr = path[1]
+                    editIndex = path[2]
+                    path[1] = currStr[0:row+editIndex-1] + self.str2[col-1] + currStr[row+editIndex:len(currStr)]
+                    path[0].append(currStr + " replace " + self.str1[row-1]  + " with " + self.str2[col-1] +  " at " + str(row+editIndex-1))
+                    path[3].append([row,col])
+                sequences.extend(allPaths)
+            except TypeError:
+                pass
+        return sequences
--- main.py
+++ main.py
@@ -1,15 +1,73 @@
 from EditDistance import EditDistance
+import numpy as np
+import os
+import sys
 
 def main():
-    str1 = input("Enter string 1: ")
-    str2 = input("Enter string 2: ")
+    ## interactive mode
+    # while True:
+    #     str1 = input("Enter string 1: ")
+    #     str2 = input("Enter string 2: ")
+    #     if str1 == str2:
+    #         print("Invalid input: strings are equivalent.")
+    #         continue
+    #     elif len(str1) == 0 or len(str2) == 0:
+    #         print("Invalid input: strings are empty")
+    #         continue
+    #     break
+    # cDel = float(input("Enter the deletion cost: "))
+    # cIns = float(input("Enter the insertion cost: "))
+    # cRep = float(input("Enter the replacement/change cost: "))
 
-    ed = EditDistance(str1, str2, 1, 1, 2)
-    cost_matrix = ed.CreateCostMatrix()
-    print(cost_matrix)
-    print(ed.FindDecisionSequence(len(str1), len(str2)))
-    print(ed.sequences)
-    print(ed.listAllSequence())
+    ## input file mode
+    cDel = 0.4
+    cIns = 0.5
+    cRep = 1.2
+    directory = 'datasets'
+    for file in os.listdir(directory):
+        try:
+            filePath = os.path.join(directory, file)
+            print()
+            print(filePath)
+            if os.path.isfile(filePath):
+                f = open(filePath, 'r')
+                str1 = f.readline().rstrip()
+                str2 = f.readline().rstrip()
+
+                if len(str1) <= 10 and len(str2) <= 10:
+                    print("String 1: " + str1)
+                    print("String 2: " + str2)
+
+                # instantiate edit distance object
+                ed = EditDistance(str1, str2, cDel, cIns, cRep)
+
+                # calculate cost matrix
+                cost_matrix = ed.CreateCostMatrix()
+                if len(str1) <= 10 and len(str2) <= 10:
+                    print("=======  Cost Matrix  =======")
+                    print(np.matrix(cost_matrix))
+                    print()
+
+                # find decision sequences
+                sequences = ed.backtrackSequence()
+                i = 1
+                if len(str1) <= 10 and len(str2) <= 10:
+                    for path in sequences:
+                        print("=======  Sequence %s  ======="%i)
+                        sequence_matrix = [[' - ' if [row,col] in path[3] else '%.1f'%cost_matrix[row][col] for col in range(len(cost_matrix[0]))] for row in range(len(cost_matrix))]
+                        if len(str1) <= 10 and len(str2) <= 10:
+                            print(np.matrix(sequence_matrix))
+                        if len(path[0]) > 0:
+                            for op in range(len(path[0])-1):
+                                print(path[0][op] + " => " + path[0][op+1].split(' ')[0])
+                            print(path[0][-1] + " => " + path[1])
+                            print()
+                            i += 1
+                print("Final cost: %.1f"%cost_matrix[-1][-1])
+        except RecursionError:
+            continue
+        
 
 if __name__ == "__main__":
+    sys.setrecursionlimit(500)
     main()
